859354e85d288e3044249e58330cc6bdd46eb82e
SpikeHD <spikegdofficial@gmail.com>
2022-09-06 15:16:30 -0700

    injection!

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,21 @@
     windows_subsystem = "windows"
 )]
 
+use std::time::Duration;
+
+#[tauri::command]
+fn eval(window: tauri::Window, contents: String) {
+    std::thread::spawn(move || {
+        std::thread::sleep(Duration::from_millis(1000));
+        window.eval(contents.as_str()).unwrap();
+    });
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![
+            eval
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,41 @@ const { invoke } = window.__TAURI__;
 
 window.addEventListener("DOMContentLoaded", () => {
   console.log('Ready!')
+
+  invoke('eval', {
+    contents: `
+      let loaded = false
+
+      let observer = new MutationObserver((mutations, obs) => {
+        const innerApp = document.querySelector('div[class*="notDevTools"]').querySelector('div[class*="app-"]')
+        const loading = Array.from(
+          innerApp.children
+        ).length === 2 || !innerApp.querySelector('div').className.includes('app')
+
+        if (!loading && !loaded) {
+          console.log('Discord is loaded!')
+
+          onClientLoad()
+
+          const nameAndDecorators = document.querySelector('div[class*="nameAndDecorators"]')
+          nameAndDecorators.innerHTML = 'Welcome to Taurcord!'
+        } else {
+          console.log('Discord not loaded...')
+        }
+      });
+
+      observer.observe(document, {
+        childList: true,
+        subtree: true
+      });
+
+      function onClientLoad() {
+        observer.disconnect()
+        observer = null
+        loaded = true
+      }
+    `
+  })
+
+  window.location.assign('https://discord.com/app')
 });
