cf64661b25cc571bd329e0a75f1712653994de48
SpikeHD <spikegdofficial@gmail.com>
2022-09-06 15:36:56 -0700

    cargo fmt

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,30 +1,28 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
 )]
 
 use std::time::Duration;
 
 #[tauri::command]
 fn eval(window: tauri::Window, contents: String) {
-    std::thread::spawn(move || {
-        std::thread::sleep(Duration::from_millis(1000));
-        window.eval(contents.as_str()).unwrap();
-    });
+  std::thread::spawn(move || {
+    std::thread::sleep(Duration::from_millis(1000));
+    window.eval(contents.as_str()).unwrap();
+  });
 }
 
 fn getPlugin(name: &str) {
-    let mut exe_dir = std::env::current_exe().unwrap();
-    exe_dir.pop();
+  let mut exe_dir = std::env::current_exe().unwrap();
+  exe_dir.pop();
 
-    let _plugins_dir = exe_dir.join("plugins");
+  let _plugins_dir = exe_dir.join("plugins");
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            eval
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![eval])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
