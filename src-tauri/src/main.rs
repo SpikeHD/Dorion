3efa2be43619956b17dadea462fd5b934b457158
SpikeHD <spikegdofficial@gmail.com>
2022-09-06 15:26:16 -0700

    begin plugin stuff

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,6 +13,13 @@ fn eval(window: tauri::Window, contents: String) {
     });
 }
 
+fn getPlugin(name: &str) {
+    let mut exe_dir = std::env::current_exe().unwrap();
+    exe_dir.pop();
+
+    let _plugins_dir = exe_dir.join("plugins");
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
