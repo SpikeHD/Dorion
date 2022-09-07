75f104b84068df839df4c5dc519d4db87988250e
SpikeHD <spikegdofficial@gmail.com>
2022-09-06 18:16:04 -0700

    go through a bunch of trouble just to set user agent

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,8 @@
   windows_subsystem = "windows"
 )]
 
-use std::{fs, time::Duration};
+use tauri::{utils::config::AppUrl, Window, WindowBuilder};
+use std::{fs, time::Duration, path::PathBuf};
 
 #[tauri::command]
 fn eval(window: tauri::Window, contents: String) {
@@ -51,8 +52,70 @@ fn load_plugins() -> String {
 }
 
 fn main() {
+  let mut context = tauri::generate_context!("tauri.conf.json");
+  let win_url = tauri::WindowUrl::App(PathBuf::from("../dist"));
+
+  context.config_mut().build.dist_dir = AppUrl::Url(win_url.clone());
+  context.config_mut().build.dev_path = AppUrl::Url(win_url.clone());
+
   tauri::Builder::default()
+    .plugin(tauri_plugin_window_state::Builder::default().build())
     .invoke_handler(tauri::generate_handler![eval, load_plugins])
-    .run(tauri::generate_context!())
+    .setup(move |app| {
+        let win = WindowBuilder::new(app, "main", win_url)
+            .title("Dorion")
+            .maximized(true)
+            .resizable(true)
+            .build()?;
+
+        set_user_agent(win);
+
+        Ok(())
+    })
+    .run(context)
     .expect("error while running tauri application");
+}
+
+// Big fat credit to icidasset & FabianLars
+// https://github.com/icidasset/diffuse/blob/main/src-tauri/src/main.rs
+fn set_user_agent(window: Window) {
+    let user_agent = "Dorion";
+
+    window.with_webview(move |webview| {
+        #[cfg(windows)]
+        unsafe {
+            use webview2_com::Microsoft::Web::WebView2::Win32::ICoreWebView2Settings2;
+            use windows::core::Interface;
+
+            let settings: ICoreWebView2Settings2 = webview
+                .controller()
+                .CoreWebView2()
+                .unwrap()
+                .Settings()
+                .unwrap()
+                .cast()
+                .unwrap();
+
+            settings
+                .SetUserAgent(user_agent)
+                .unwrap();
+        }
+
+        #[cfg(target_os = "linux")]
+        {
+            use webkit2gtk::{WebViewExt, SettingsExt};
+            let webview = webview.inner();
+            let settings = webview.settings().unwrap();
+            settings.set_user_agent(Some(user_agent));
+        }
+
+        // untested
+        #[cfg(target_os = "macos")]
+        unsafe {
+            use objc::{msg_send, sel, sel_impl};
+            use objc_foundation::{NSString, INSString};
+            let agent = NSString::from_str(user_agent);
+            let () = msg_send![webview.inner(), setCustomUserAgent: agent];
+        }
+    }).unwrap();
 }
