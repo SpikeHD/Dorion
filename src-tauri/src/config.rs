70d317dd39948754ae76f0a7a2072b0622061f42
SpikeHD <spikegdofficial@gmail.com>
2022-09-08 13:08:45 -0700

    fix default zoom

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -18,7 +18,7 @@ pub fn init() {
   if fs::metadata(&config_file).is_err() {
     fs::write(
       config_file,
-      r#"{ "theme": "none", "zoom": 100, "client_type": "default" }"#,
+      r#"{ "theme": "none", "zoom": "1.0", "client_type": "default" }"#,
     )
     .unwrap();
   }
