40fcd9b7ab173423c41c5c908bcc0f36acbca8d6
SpikeHD <spikegdofficial@gmail.com>
2023-03-15 17:53:57 -0700

    fix generated checkboxes

--- src-tauri/injection/injection.js
+++ src-tauri/injection/injection.js
@@ -362,20 +362,24 @@ function createSlider(label, id, onchange) {
   const div = document.createElement('div')
   const htmlStr = `
   <label for="${id}">${label}</label>
-  <div class="control-1fl03-">
-    <div class="container-2nx-BQ" style="opacity: 1;">
+  <div class="control-10qYax">
+    <div class="container-1QtPKm default-colors" style="opacity: 1; background-color: rgb(128, 132, 142);">
       <!-- Checkbox/slider SVG -->
-      <svg class="slider-32CRPX" viewBox="0 0 28 20" preserveAspectRatio="xMinYMid meet" style="left: 12px;">
-        <rect fill="white" x="4" y="0" height="20" width="20" rx="10"></rect>
-        <svg viewBox="0 0 20 20" fill="none">
-          <path fill="rgba(59, 165, 92, 1)" d="M7.89561 14.8538L6.30462 13.2629L14.3099 5.25755L15.9009 6.84854L7.89561 14.8538Z"></path>
-          <path fill="rgba(59, 165, 92, 1)" d="M4.08643 11.0903L5.67742 9.49929L9.4485 13.2704L7.85751 14.8614L4.08643 11.0903Z"></path>
+      <svg class="slider-HJFN2i" viewBox="0 0 28 20" preserveAspectRatio="xMinYMid meet" aria-hidden="true"
+        style="left: -3px;">
+        <rect fill="white" x="4" y="0" height="20" width="20" rx="10"></rect><svg viewBox="0 0 20 20"
+          fill="none">
+          <path fill="rgba(128, 132, 142, 1)"
+            d="M5.13231 6.72963L6.7233 5.13864L14.855 13.2704L13.264 14.8614L5.13231 6.72963Z"></path>
+          <path fill="rgba(128, 132, 142, 1)"
+            d="M13.2704 5.13864L14.8614 6.72963L6.72963 14.8614L5.13864 13.2704L13.2704 5.13864Z"></path>
         </svg>
       </svg>
       <!-- End SVG -->
-      <input type="checkbox" class="input-2XRLou" id="${id}" name="${id}" />
+      <input type="checkbox" class="input-125oad" id="${id}" name="${id}" />
     </div>
-  </div>`
+  </div>
+  `
 
   div.innerHTML = htmlStr
 
