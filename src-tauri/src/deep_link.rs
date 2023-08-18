e78c69f5365fcfe1d33965b9686a00a0bf3cfd51
SpikeHD <spikegdofficial@gmail.com>
2023-08-17 18:36:29 -0700

    fix(all): properly show window

--- src-tauri/src/deep_link.rs
+++ src-tauri/src/deep_link.rs
@@ -1,5 +1,6 @@
 pub fn register_deep_link_handler(win: tauri::Window) {
   tauri_plugin_deep_link::register("dorion", move |_| {
+    win.show().unwrap();
     win.set_focus().unwrap();
     win.unminimize().unwrap();
   }).unwrap();
