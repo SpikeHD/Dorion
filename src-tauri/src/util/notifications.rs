de8b58fb3797e9e29955fc7567dd1f68b33a379a
SpikeHD <spikegdofficial@gmail.com>
2023-08-18 18:12:17 -0700

    fix(macos): notifications stuff

--- src-tauri/src/util/notifications.rs
+++ src-tauri/src/util/notifications.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use tauri::{Icon, Manager};
 
 #[cfg(not(target_os = "macos"))]
-pub fn set_notif_icon(window: tauri::Window, amount: u16) {
+pub fn set_notif_icon(window: &tauri::Window, amount: u16) {
   let icon_num = if amount > 9 { 9 } else { amount };
 
   // We do not have a zero icon, set back to regular icon
@@ -42,11 +42,12 @@ pub fn set_notif_icon(window: tauri::Window, amount: u16) {
 #[cfg(target_os = "macos")]
 pub unsafe fn set_notif_icon(_window: &tauri::Window, amount: u16) {
   use cocoa::{appkit::NSApp, base::nil, foundation::NSString};
+  use objc::{sel, sel_impl, msg_send};
 
-  let label = if count == 0 {
+  let label = if amount == 0 {
     nil
   } else {
-      NSString::alloc(nil).init_str(&format!("{}", count))
+      NSString::alloc(nil).init_str(&format!("{}", amount))
   };
   let dock_tile: cocoa::base::id = msg_send![NSApp(), dockTile];
   let _: cocoa::base::id = msg_send![dock_tile, setBadgeLabel: label];
@@ -57,6 +58,6 @@ pub fn notif_count(window: tauri::Window, amount: u16) {
   println!("Setting notification count: {}", amount);
 
   unsafe {
-    set_notif_icon(window, amount);
+    set_notif_icon(&window, amount);
   };
 }
