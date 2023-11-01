d5b8ec27d66e1288ea43396a9b485829c04a8277
SpikeHD <spikegdofficial@gmail.com>
2023-11-01 08:39:21 -0700

    fix(all): use a detectable.json that doesn't include lms.exe

--- src-tauri/src/functionality/rpc.rs
+++ src-tauri/src/functionality/rpc.rs
@@ -2,7 +2,7 @@ use rsrpc::RPCServer;
 
 pub fn start_rpc_server() {
   let detectable = reqwest::blocking::get(
-    "https://raw.githubusercontent.com/OpenAsar/arrpc/main/src/process/detectable.json",
+    "https://gist.githubusercontent.com/SpikeHD/209bd9b17c97f45dc5be4803c748726f/raw/ddf8ed33621933b4e3c58cf1113e1679ab9fd9b5/dorion_detectable.json",
   )
   .unwrap()
   .text()
