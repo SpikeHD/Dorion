3b9f0f89b51aa536a06ca9aa4e9b41289296448d
SpikeHD <spikegdofficial@gmail.com>
2023-12-25 00:46:17 -0800

    fix(all): blur crash prevention

--- src-tauri/src/window/blur.rs
+++ src-tauri/src/window/blur.rs
@@ -34,9 +34,9 @@ pub fn apply_effect(win: tauri::Window, effect: &str) {
     use window_vibrancy::{apply_blur, apply_acrylic, apply_mica};
 
     match effect {
-      "blur" => apply_blur(win, Some((18, 18, 18, 125))).expect("Unsupported platform! How... did you do that?"),
-      "acrylic" => apply_acrylic(win, Some((18, 18, 18, 125))).expect("Unsupported platform! How... did you do that?"),
-      "mica" => apply_mica(win, None).expect("Unsupported platform! How... did you do that?"),
+      "blur" => apply_blur(win, Some((18, 18, 18, 125))).unwrap_or_default(),
+      "acrylic" => apply_acrylic(win, Some((18, 18, 18, 125))).unwrap_or_default(),
+      "mica" => apply_mica(win, None).unwrap_or_default(),
       _ => ()
     }
   }
@@ -46,7 +46,7 @@ pub fn apply_effect(win: tauri::Window, effect: &str) {
     use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
 
     match effect {
-      "vibrancy" => apply_vibrancy(win, NSVisualEffectMaterial::HudWindow, None, None).expect("Unsupported platform! How... did you do that?"),
+      "vibrancy" => apply_vibrancy(win, NSVisualEffectMaterial::HudWindow, None, None).unwrap_or_default(),
       _ => {}
     }
   }
@@ -58,8 +58,8 @@ pub fn remove_effect(win: tauri::Window) {
   {
     use window_vibrancy::{clear_blur, clear_acrylic, clear_mica};
 
-    clear_blur(&win).expect("Unsupported platform! How... did you do that?");
-    clear_acrylic(&win).expect("Unsupported platform! How... did you do that?");
-    clear_mica(win).expect("Unsupported platform! How... did you do that?");
+    clear_blur(&win).unwrap_or_default();
+    clear_acrylic(&win).unwrap_or_default();
+    clear_mica(win).unwrap_or_default();
   }
 }
