a2fca19a93472c94129cfaabdf6a13195398e303
SpikeHD <spikegdofficial@gmail.com>
2024-01-03 21:01:51 -0800

    fix(all): expose window zoom

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -173,6 +173,7 @@ fn main() {
       window::blur::remove_effect,
       window_helpers::remove_top_bar,
       window_helpers::set_clear_cache,
+      window_helpers::window_zoom_level,
     ])
     .on_window_event(|event| match event.event() {
       tauri::WindowEvent::Destroyed { .. } => {
