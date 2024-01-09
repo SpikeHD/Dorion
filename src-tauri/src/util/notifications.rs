090296488d86bbcac05def879787df2bf5b13e61
SpikeHD <spikegdofficial@gmail.com>
2024-01-08 23:38:05 -0800

    fix(all): reorder

--- src-tauri/src/util/notifications.rs
+++ src-tauri/src/util/notifications.rs
@@ -57,57 +57,17 @@ fn send_notification_internal(win: tauri::Window, title: String, body: String, i
     .unwrap_or_default();
 }
 
-#[cfg(target_os = "linux")]
-pub fn set_notif_icon(_window: &tauri::Window, _amount: u16) {
-  // This doesn't work right now womp womp
+#[tauri::command]
+pub fn notif_count(window: tauri::Window, amount: u16) {
+  log(format!("Setting notification count: {}", amount));
 
-  // let icon_num = if amount > 9 { 9 } else { amount };
+  #[cfg(not(target_os = "linux"))]
+  unsafe {
+    set_notif_icon(&window, amount);
+  }
 
-  // // We do not have a zero icon, set back to regular icon
-  // if icon_num < 1 {
-  //   let mut icon_path = PathBuf::from("icons/icon");
-  //   icon_path.set_extension("ico");
-
-  //   window
-  //     .set_icon(Icon::File(
-  //       window
-  //         .app_handle()
-  //         .path_resolver()
-  //         .resolve_resource(icon_path)
-  //         .unwrap(),
-  //     ))
-  //     .unwrap_or(());
-  //   return;
-  // }
-
-  // let icon_name = format!("icon_{}", icon_num);
-  // let mut icon_path = PathBuf::from("icons/").join(icon_name);
-  // icon_path.set_extension("ico");
-
-  // window
-  //   .set_icon(Icon::File(
-  //     window
-  //       .app_handle()
-  //       .path_resolver()
-  //       .resolve_resource(icon_path)
-  //       .unwrap(),
-  //   ))
-  //   .unwrap_or(());
-}
-
-// https://github.com/tauri-apps/tauri/issues/4489#issuecomment-1170050529
-#[cfg(target_os = "macos")]
-pub unsafe fn set_notif_icon(_window: &tauri::Window, amount: u16) {
-  use cocoa::{appkit::NSApp, base::nil, foundation::NSString};
-  use objc::{msg_send, sel, sel_impl};
-
-  let label = if amount == 0 {
-    nil
-  } else {
-    NSString::alloc(nil).init_str(&format!("{}", amount))
-  };
-  let dock_tile: cocoa::base::id = msg_send![NSApp(), dockTile];
-  let _: cocoa::base::id = msg_send![dock_tile, setBadgeLabel: label];
+  #[cfg(target_os = "linux")]
+  set_notif_icon(&window, amount);
 }
 
 #[cfg(target_os = "windows")]
@@ -198,15 +158,20 @@ pub unsafe fn set_notif_icon(window: &tauri::Window, amount: u16) {
   CoUninitialize();
 }
 
-#[tauri::command]
-pub fn notif_count(window: tauri::Window, amount: u16) {
-  log(format!("Setting notification count: {}", amount));
+// https://github.com/tauri-apps/tauri/issues/4489#issuecomment-1170050529
+#[cfg(target_os = "macos")]
+pub unsafe fn set_notif_icon(_window: &tauri::Window, amount: u16) {
+  use cocoa::{appkit::NSApp, base::nil, foundation::NSString};
+  use objc::{msg_send, sel, sel_impl};
 
-  #[cfg(not(target_os = "linux"))]
-  unsafe {
-    set_notif_icon(&window, amount);
-  }
-
-  #[cfg(target_os = "linux")]
-  set_notif_icon(&window, amount);
+  let label = if amount == 0 {
+    nil
+  } else {
+    NSString::alloc(nil).init_str(&format!("{}", amount))
+  };
+  let dock_tile: cocoa::base::id = msg_send![NSApp(), dockTile];
+  let _: cocoa::base::id = msg_send![dock_tile, setBadgeLabel: label];
 }
+
+#[cfg(target_os = "linux")]
+pub fn set_notif_icon(_window: &tauri::Window, _amount: u16) {}
