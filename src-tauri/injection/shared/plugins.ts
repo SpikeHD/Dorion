3fb96c7c41fa4dcbab5a1b6e83c33d6efac94c6a
SpikeHD <spikegdofficial@gmail.com>
2024-02-20 14:43:50 -0800

    fix: no option check

--- src-tauri/injection/shared/plugins.ts
+++ src-tauri/injection/shared/plugins.ts
@@ -15,7 +15,7 @@ export async function ensurePlugins() {
       'https://spikehd.github.io/shelter-plugins/dorion-fullscreen/',
   }
 
-  if (window.__DORION_CONFIG__?.client_plugins !== null && !window.__DORION_CONFIG__?.client_plugins) {
+  if (window.__DORION_CONFIG__?.client_plugins != null && !window.__DORION_CONFIG__?.client_plugins) {
     console.log('[Ensure Plugins] Client plugins are disabled. I hope you know what you are doing!')
     return
   }
