1c35dbdea173664ee5f59bc0eb67a932e8fe109a
SpikeHD <spikegdofficial@gmail.com>
2024-07-11 22:17:54 -0700

    fix: reimpl deep link listen

--- src-tauri/src/deep_link.rs
+++ src-tauri/src/deep_link.rs
@@ -3,18 +3,20 @@ use tauri::Manager;
 use crate::log;
 
 pub fn register_deep_link_handler(app: &tauri::AppHandle) {
-  app.listen("dorion://open", |url| {
-    // TODO do this
-    // let win = app.get_webview_window("main");
+  let handle = app.clone();
+  app.listen("dorion://open", move |url| {
+    log!("Received deep link message: {:?}", url);
 
-    // if win.is_none() {
-    //   return;
-    // }
+    let win = handle.get_webview_window("main");
 
-    // let win = win.unwrap();
+    if win.is_none() {
+      return;
+    }
 
-    // win.show().unwrap_or_default();
-    // win.set_focus().unwrap_or_default();
-    // win.unminimize().unwrap_or_default();
+    let win = win.unwrap();
+
+    win.show().unwrap_or_default();
+    win.set_focus().unwrap_or_default();
+    win.unminimize().unwrap_or_default();
   });
 }
