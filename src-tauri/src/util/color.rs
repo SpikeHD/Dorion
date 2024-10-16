d488550eeef70f16d35fefe5b891fcde9b0b4194
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-10-16 11:58:04 -0700

    fix: capture initial value

--- src-tauri/src/util/color.rs
+++ src-tauri/src/util/color.rs
@@ -1,11 +1,18 @@
-use mundy::{self, AccentColor, Interest, Preferences};
+use std::sync::Mutex;
+use mundy::{self, Interest, Preferences};
 use tauri::Emitter;
 
 use crate::log;
 
+static ACCENT_COLOR: Mutex<Option<(u8, u8, u8, u8)>> = Mutex::new(None);
+
+pub fn set_accent_color(r: u8, g: u8, b: u8, a: u8) {
+  ACCENT_COLOR.lock().unwrap().replace((r, g, b, a));
+}
+
 pub fn start_os_accent_subscriber(win: &tauri::WebviewWindow) {
   let win = win.clone();
-  Preferences::subscribe(Interest::AccentColor, move |prefs| {
+  Preferences::subscribe(Interest::All, move |prefs| {
     let accent = prefs.accent_color.0;
 
     if let Some(accent) = accent {
@@ -18,6 +25,18 @@ pub fn start_os_accent_subscriber(win: &tauri::WebviewWindow) {
       win.emit("os_accent_update", format!("rgba({:.0}, {:.0}, {:.0}, {:.0})", r, g, b, a)).unwrap_or_else(|e| {
         log!("Error emitting os_accent_update event: {}", e);
       });
+
+      set_accent_color(r as u8, g as u8, b as u8, a as u8);
     }
   });
+}
+
+pub fn _get_os_accent() -> String {
+  let accent = ACCENT_COLOR.lock().unwrap();
+
+  if let Some(accent) = accent.as_ref() {
+    return format!("rgba({}, {}, {}, {})", accent.0, accent.1, accent.2, accent.3);
+  }
+  
+  "".to_string()
 }
