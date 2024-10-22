64f5f05559458fd5b1a49367720117d83e16e150
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-10-21 20:53:16 -0700

    fix: more cross compilation stuff

--- src-tauri/extension_webkit/toolchain-arm64.cmake
+++ src-tauri/extension_webkit/toolchain-arm64.cmake
@@ -5,7 +5,7 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 # Cross-compilation toolchain
 set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
-set(CMAKE_SYSROOT /usr/lib/aarch64-linux-gnu)
+# set(CMAKE_SYSROOT /usr/lib/aarch64-linux-gnu)
 
 # Library paths
 set(CMAKE_FIND_ROOT_PATH /usr/lib/aarch64-linux-gnu)
--- src-tauri/extension_webkit/toolchain-armv7.cmake
+++ src-tauri/extension_webkit/toolchain-armv7.cmake
@@ -5,7 +5,7 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 # Cross-compilation toolchain
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
-set(CMAKE_SYSROOT /usr/lib/arm-linux-gnueabihf)
+# set(CMAKE_SYSROOT /usr/lib/arm-linux-gnueabihf)
 
 # Library paths
 set(CMAKE_FIND_ROOT_PATH /usr/lib/arm-linux-gnueabihf)
