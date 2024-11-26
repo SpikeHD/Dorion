ad06bc24ea18670be86b56db49354c7401bcc881
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-11-26 12:34:55 -0800

    fix: remove tauri devtools

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -127,7 +127,6 @@ fn main() {
   let mut builder = tauri::Builder::default()
     .plugin(tauri_plugin_http::init())
     .plugin(tauri_plugin_shell::init())
-    .plugin(tauri_plugin_devtools::init())
     .plugin(tauri_plugin_autostart::init(
       tauri_plugin_autostart::MacosLauncher::LaunchAgent,
       Some(vec!["--startup"]),
