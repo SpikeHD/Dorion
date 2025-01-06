ea1a290527993d9f636a0d89777f7100a54dfde9
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2025-01-06 07:36:09 -0800

    fix: macos notification badges

--- src-tauri/src/util/notifications.rs
+++ src-tauri/src/util/notifications.rs
@@ -128,59 +128,88 @@ fn send_notification_internal_windows(
 #[tauri::command]
 pub fn notification_count(window: tauri::WebviewWindow, amount: i64) {
   log!("Setting notification count: {}", amount);
-
-  #[cfg(not(target_os = "windows"))]
-  window.set_badge_count(Some(amount)).unwrap_or_default();
-
-  #[cfg(target_os = "windows")]
-  {
-    if amount == 0 {
-      window.set_overlay_icon(None).unwrap_or_default();
-    } else {
-      use include_flate::flate;
-      use tauri::image::Image;
-
-      // Include icons
-      flate!(static ICO_SOME: [u8] from "./icons/notifications/some_48.png");
-      flate!(static ICO_1: [u8] from "./icons/notifications/1_48.png");
-      flate!(static ICO_2: [u8] from "./icons/notifications/2_48.png");
-      flate!(static ICO_3: [u8] from "./icons/notifications/3_48.png");
-      flate!(static ICO_4: [u8] from "./icons/notifications/4_48.png");
-      flate!(static ICO_5: [u8] from "./icons/notifications/5_48.png");
-      flate!(static ICO_6: [u8] from "./icons/notifications/6_48.png");
-      flate!(static ICO_7: [u8] from "./icons/notifications/7_48.png");
-      flate!(static ICO_8: [u8] from "./icons/notifications/8_48.png");
-      flate!(static ICO_9: [u8] from "./icons/notifications/9_48.png");
-
-      let ico = match amount {
-        -1 => ICO_SOME.as_ref(),
-        1 => ICO_1.as_ref(),
-        2 => ICO_2.as_ref(),
-        3 => ICO_3.as_ref(),
-        4 => ICO_4.as_ref(),
-        5 => ICO_5.as_ref(),
-        6 => ICO_6.as_ref(),
-        7 => ICO_7.as_ref(),
-        8 => ICO_8.as_ref(),
-        9 => ICO_9.as_ref(),
-        // more than 9, just stay at 9
-        _ => ICO_9.as_ref(),
-      };
-
-      let converted = Image::from_bytes(ico);
-
-      if let Ok(converted) = converted {
-        window.set_overlay_icon(Some(converted)).unwrap_or_default();
-      } else {
-        log!("Failed to convert notification icon: {:?}", converted.err());
-        window.set_overlay_icon(None).unwrap_or_default();
-      }
-    }
-  }
+  
+  notification_count_inner(&window, amount);
 
   // If the tray state is unread or default,
   if TrayIcon::from_usize(TRAY_STATE.load(Ordering::Relaxed)).is_overwrite() {
     let state = if amount == 0 { "default" } else { "unread" };
     set_tray_icon(window.app_handle().to_owned(), state.to_string());
+  }
+}
+
+#[cfg(target_os = "linux")]
+fn notification_count_inner(window: &tauri::WebviewWindow, amount: i64) {
+  window.set_badge_count(if amount < 0 { None } else { Some(amount) }).unwrap_or_default();
+}
+
+#[cfg(target_os = "windows")]
+fn notification_count_inner(window: &tauri::WebviewWindow, amount: i64) {
+  if amount == 0 {
+    window.set_overlay_icon(None).unwrap_or_default();
+  } else {
+    use include_flate::flate;
+    use tauri::image::Image;
+
+    // Include icons
+    flate!(static ICO_SOME: [u8] from "./icons/notifications/some_48.png");
+    flate!(static ICO_1: [u8] from "./icons/notifications/1_48.png");
+    flate!(static ICO_2: [u8] from "./icons/notifications/2_48.png");
+    flate!(static ICO_3: [u8] from "./icons/notifications/3_48.png");
+    flate!(static ICO_4: [u8] from "./icons/notifications/4_48.png");
+    flate!(static ICO_5: [u8] from "./icons/notifications/5_48.png");
+    flate!(static ICO_6: [u8] from "./icons/notifications/6_48.png");
+    flate!(static ICO_7: [u8] from "./icons/notifications/7_48.png");
+    flate!(static ICO_8: [u8] from "./icons/notifications/8_48.png");
+    flate!(static ICO_9: [u8] from "./icons/notifications/9_48.png");
+
+    let ico = match amount {
+      -1 => ICO_SOME.as_ref(),
+      1 => ICO_1.as_ref(),
+      2 => ICO_2.as_ref(),
+      3 => ICO_3.as_ref(),
+      4 => ICO_4.as_ref(),
+      5 => ICO_5.as_ref(),
+      6 => ICO_6.as_ref(),
+      7 => ICO_7.as_ref(),
+      8 => ICO_8.as_ref(),
+      9 => ICO_9.as_ref(),
+      // more than 9, just stay at 9
+      _ => ICO_9.as_ref(),
+    };
+
+    let converted = Image::from_bytes(ico);
+
+    if let Ok(converted) = converted {
+      window.set_overlay_icon(Some(converted)).unwrap_or_default();
+    } else {
+      log!("Failed to convert notification icon: {:?}", converted.err());
+      window.set_overlay_icon(None).unwrap_or_default();
+    }
+  }
+}
+
+#[cfg(target_os = "macos")]
+fn notification_count_inner(_window: &tauri::WebviewWindow, amount: i64) {
+  use objc2_app_kit::NSApp;
+  use objc2_foundation::{MainThreadMarker, NSString};
+
+  let label = if amount > 0 {
+    Some(NSString::from_str(&format!("{amount}")))
+  } else if amount == -1 {
+    Some(NSString::from_str("●"))
+  } else {
+    None
+  };
+
+  if let Some(thread) = MainThreadMarker::new() {
+    unsafe {
+      let app = NSApp(thread);
+      let dock_tile = app.dockTile();
+      dock_tile.setBadgeLabel(label.as_deref());
+      dock_tile.display();
+    }
+  } else {
+    log!("Failed to mark main thread!");
   }
 }
