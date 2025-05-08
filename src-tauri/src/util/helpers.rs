da2b5196fbc9b44efbd37d5d42875d6a8cde8804
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2025-05-08 13:55:58 -0700

    fix: switch from custom open commands top `open` crate

--- src-tauri/src/util/helpers.rs
+++ src-tauri/src/util/helpers.rs
@@ -57,15 +57,7 @@ pub fn open_extensions() {
 }
 
 fn open_folder(path: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
-  #[cfg(target_os = "windows")]
-  Command::new("explorer").arg(path).spawn()?;
-
-  #[cfg(target_os = "macos")]
-  Command::new("open").arg(path).spawn()?;
-
-  #[cfg(target_os = "linux")]
-  Command::new("xdg-open").arg(path).spawn()?;
-
+  open::that(path)?;
   Ok(())
 }
 
