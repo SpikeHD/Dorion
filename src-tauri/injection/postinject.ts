1359796608490b626088803e44a6786a8aa77c73
Small_Ku <small_ku@outlook.com>
2025-12-19 22:14:06 +0800

    fix: reduce querySelector loops
    introduced the over-complicated `waitForElmEx` function from SpikeHD/shelter-plugins#60

--- src-tauri/injection/postinject.ts
+++ src-tauri/injection/postinject.ts
@@ -1,45 +1,21 @@
 import { applyExtraCSS } from './shared/ui'
+import { waitForDom, waitForElmEx } from './shared/util';
 import { initWindowsKeybinds } from './shared/windows_keybinds'
 
-let loaded = false
+(async () => {
+  await waitForElmEx([['>div[class*=-content]', '>div[class*=content_]']]);
 
-/**
- * Observer that lets us know when Discord is loaded
- */
-const observer = new MutationObserver(() => {
-  const content = document.querySelector('div[class*="content_"]')
+  console.log('Discord is loaded!')
 
-  if (content && !loaded) {
-    console.log('Discord is loaded!')
-
-    loaded = true
-
-    // Ensure top bar exists if we want it
-    if (window.__DORION_CONFIG__.use_native_titlebar)
-      window.__TAURI__.core.invoke('set_decorations', { enable: true }).catch(_e => {}) // This is allowed to fail
-
-    onClientLoad()
-
-    // The comment ahead is read by tauri and used to insert theme injection code
-
-    /*! __THEMES__ */
-  } else {
-    console.log('Discord not loaded...')
-  }
-})
-
-observer.observe(document, {
-  childList: true,
-  subtree: true,
-})
-
-/**
- * Run when the client is "loaded"
- */
-function onClientLoad() {
-  observer.disconnect()
+  // Ensure top bar exists if we want it
+  if (window.__DORION_CONFIG__.use_native_titlebar)
+    window.__TAURI__.core.invoke('set_decorations', { enable: true }).catch(_e => { }) // This is allowed to fail
 
   // Load up our extra css
   applyExtraCSS()
   initWindowsKeybinds()
-}
+
+  // The comment ahead is read by tauri and used to insert theme injection code
+
+  /*! __THEMES__ */
+})();
